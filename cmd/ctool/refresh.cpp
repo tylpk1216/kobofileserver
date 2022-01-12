35ee144fef49e81bc874cef26a495bc18a078467
tylpk1216 <tylpk1216@gmail.com>
2022-01-12 15:27:32 +0800

    Update test tool.

--- cmd/ctool/refresh.cpp
+++ cmd/ctool/refresh.cpp
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    int timeOutSec = 0;
+    int timeoutSec = 0;
     if (argc == 2) {
-        timeOutSec = atoi(argv[1]);
+        timeoutSec = atoi(argv[1]);
     }
 
-    Cli cli(&app, timeOutSec);
+    Cli cli(&app, timeoutSec);
     QTimer::singleShot(0, &cli, SLOT(start()));
 
     return app.exec();
