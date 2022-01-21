f26b35ff1c496f9b6ce1a21f84f45411e29e26e7
tylpk1216 <tylpk1216@gmail.com>
2022-01-22 00:28:39 +0800

    Fix showing issue.

--- cmd/scripts/kobofileserver.sh
+++ cmd/scripts/kobofileserver.sh
@@ -6,4 +6,4 @@ FBINK=/usr/bin/fbink
 
 $WORK_DIR/kobofileserver >>$WORK_DIR/log.txt 2>&1 &
 
-[ -e "$FBINK" ] && $FBINK -g file=$WORK_DIR/qrcode.png,halign=CENTER,valign=CENTER -W A2
+[ -e "$FBINK" ] && $FBINK -g file=$WORK_DIR/qrcode.png,halign=CENTER,valign=CENTER -f
