e9b8c4c2b6f2a6b11736aafd1cf801627fa77b09
tylpk1216 <tylpk1216@gmail.com>
2022-01-22 00:07:58 +0800

    Update run script.

--- cmd/scripts/kobofileserver.sh
+++ cmd/scripts/kobofileserver.sh
@@ -4,8 +4,6 @@ WORK_DIR="${0%/*}"
 
 FBINK=/usr/bin/fbink
 
-$WORK_DIR/kobofileserver& >>$WORK_DIR/log.txt 2>&1
-
-sleep 2s
+$WORK_DIR/kobofileserver >>$WORK_DIR/log.txt 2>&1 &
 
 [ -e "$FBINK" ] && $FBINK -g file=$WORK_DIR/qrcode.png,halign=CENTER,valign=CENTER -W A2
